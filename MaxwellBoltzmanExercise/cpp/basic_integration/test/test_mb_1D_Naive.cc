22f8f858208a1a0b475c1b87635c59fcd6318b9d
Xander Bowen Struyk <xbs0124@quser31.quest.it.northwestern.edu>
2023-04-26 16:16:16 -0500

    Added the Maxwell Boltzmann dists to basic integration

---
+++ MaxwellBoltzmanExercise/cpp/basic_integration/test/test_mb_1D_Naive.cc
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "circle_functions.hh"
+#include "integration_Naive.hh"
+#include "pdfs.hh"
+
+
+int 
+main(int argc, char** argv ) 
+{ 
+
+  unsigned seed = strtoul(argv[1],NULL,10);
+  unsigned long ntrials = strtoul(argv[2],NULL,10);
+  unsigned verbose=0;
+  if( argc > 3 ) { 
+    verbose = 1;
+  }
+
+  Points2D points;
+  double params[] = { 1. }; // a=1.0
+  srand(seed);
+
+  double area;
+  if( verbose ) area=integrate_1D_Naive(&pdf_maxwell_boltzmann, (double*)&params, 0., 5., ntrials, &points);
+  else          area=integrate_1D_Naive(&pdf_maxwell_boltzmann, (double*)&params, 0., 5., ntrials );
+
+
+  // with estimated error
+  //fprintf( stderr, "Area: %lf\t4xArea: %lf\terror: %lf\tNtrials: %lu\n", 
+  //area, 4*area, 4*binomial_error(ntrials, area*ntrials), ntrials);
+
+  // with true error
+  fprintf( stderr, "Area: %lf\t4xArea: %lf\terror: %lf\tNtrials: %lu\n", 
+	   area, 4*area, binomial_error(ntrials, ntrials/25.), ntrials);
+
+}
